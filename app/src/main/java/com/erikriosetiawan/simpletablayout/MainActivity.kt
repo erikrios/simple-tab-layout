29fea95f7232a6fd205da81d492eb2373ce63272
erikrios <erikriosetiawan49@gmail.com>
2020-02-14 11:21:21 +0700

    Integrated the tab layout with view pagaer 2

--- app/src/main/java/com/erikriosetiawan/simpletablayout/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/simpletablayout/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.erikriosetiawan.simpletablayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.simpletablayout.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val fragments =
+            mutableListOf(OrderFragment(), PendingFragment(), DeliveredFragment())
+        binding.viewPager2.adapter = MainPagerAdapter(this, fragments)
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2) { tab, position ->
+            val title = listOf("Order", "Pending", "Delivered")
+            tab.text = title[position]
+        }.attach()
     }
 }
